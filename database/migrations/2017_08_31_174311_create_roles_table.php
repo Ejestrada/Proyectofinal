50c3f319a667f766f391f95f84ed8e936d6b6464
Juan Pedro <juanpedrotzul@gmail.com>
2017-08-31 17:55:15 -0600

    Se agrego la migracion para los roles y usuarios

--- database/migrations/2017_08_31_174311_create_roles_table.php
+++ database/migrations/2017_08_31_174311_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',100);
-            $table->string('second_name',100);
-            $table->string('email',100)->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nombre',50);
+            $table->string('desc',150);
+            $table->boolean('activo')->default(true);
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 }
