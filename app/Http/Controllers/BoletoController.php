6af7ce9764cc6e6b77b6473d03c0b4cdbab34b40
Juan Pedro <juanpedrotzul@gmail.com>
2017-08-31 21:32:59 -0600

    Migraciones completas

---
+++ app/Http/Controllers/BoletoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\boleto;
+use Illuminate\Http\Request;
+
+class BoletoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\boleto  $boleto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(boleto $boleto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\boleto  $boleto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(boleto $boleto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\boleto  $boleto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, boleto $boleto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\boleto  $boleto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(boleto $boleto)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RangoEdadeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\rango_edade;
+use Illuminate\Http\Request;
+
+class RangoEdadeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\rango_edade  $rango_edade
+     * @return \Illuminate\Http\Response
+     */
+    public function show(rango_edade $rango_edade)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\rango_edade  $rango_edade
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(rango_edade $rango_edade)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\rango_edade  $rango_edade
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, rango_edade $rango_edade)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\rango_edade  $rango_edade
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(rango_edade $rango_edade)
+    {
+        //
+    }
+}
